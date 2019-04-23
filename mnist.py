fd6a09e1370d60dccf538961f74d6aacc562d495
Ronnypetson Souza da Silva <ronnypetson@cpqd.com.br>
2019-04-23 16:11:18 -0300

    banana aadsd dsda aaaaaaaa aa ..aa

--- mnist.py
+++ mnist.py
@@ -15,14 +15,14 @@ class Net(nn.Module):
         self.csa1 = CSA(1,8,783)
         #self.csa2 = nn.Conv2d(8,8,3)
         self.csa2 = CSA(8,8,783)
-        self.fc1 = nn.Linear(784, 500)
+        self.fc1 = nn.Linear(8*784, 500)
         self.fc2 = nn.Linear(500, 10)
 
     def forward(self, x):
         x = F.relu(self.csa1(x))
         x = F.relu(self.csa2(x))
         #print(x.size())
-        x = x.view(-1, 784)
+        x = x.view(-1,8*784)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
