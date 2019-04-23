a9d75675ea1435363f99727de5a28f25ee998ab7
Ronnypetson Souza da Silva <ronnypetson@cpqd.com.br>
2019-04-23 16:17:12 -0300

    banana njan

--- mnist.py
+++ mnist.py
@@ -19,9 +19,9 @@ class Net(nn.Module):
         self.fc2 = nn.Linear(500, 10)
 
     def forward(self, x):
-        print(x[0])
         x = F.relu(self.csa1(x))
         x = F.relu(self.csa2(x))
+        print(x[0])
         #print(x.size())
         x = x.view(-1,8*784)
         x = F.relu(self.fc1(x))
